540d6673c1f2cac9b07d55d532a593d875d9716b
dansadmin@homail.com <dansadmin@homail.com>
2022-08-21 21:07:13 +0200

    adding more examples func and pipes

---
+++ 03-func/02/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+	"time"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+
+}
+
+func monthdayYear(t time.Time) string {
+	return t.Format("02-01-2006")
+}
+
+var fm = template.FuncMap{
+	"dateMDY": monthdayYear,
+}
+
+func main() {
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
