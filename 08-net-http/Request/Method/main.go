30d5d166643b150bd8c884c9a2890ab0d4fa2bda
dansadmin@homail.com <dansadmin@homail.com>
2022-08-30 21:09:08 +0200

    adding more examples for server net http

---
+++ 08-net-http/Request/Method/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type data struct {
+	Method      string
+	Submissions url.Values
+}
+
+var tpl *template.Template
+
+type hotdog int
+
+func (h hotdog) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	d := data{
+		Method:      req.Method,
+		Submissions: req.Form,
+	}
+
+	err = tpl.ExecuteTemplate(res, "index.gohtml", d)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+
+	http.ListenAndServe("127.0.0.1:8080", d)
+}
