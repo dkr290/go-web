838af949a9e3e7f86586519d8e4b1cfdde07a35d
Danail <d_krasimirov@yahoo.com>
2022-09-05 21:47:35 +0200

    some more webdev

---
+++ 09-passing-data/03-form/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+type person struct {
+	FirstName  string
+	LastName   string
+	Subscribed bool
+}
+
+func init() {
+
+	tpl = template.Must(template.ParseGlob("templates/*"))
+
+}
+
+func main() {
+
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe("127.0.0.1:8080", nil)
+
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+
+	f := req.FormValue("first")
+	l := req.FormValue("last")
+	s := req.FormValue("subscribe") == "on"
+
+	p := person{
+		FirstName:  f,
+		LastName:   l,
+		Subscribed: s,
+	}
+
+	err := tpl.ExecuteTemplate(res, "index.gohtml", p)
+
+	if err != nil {
+		http.Error(res, err.Error(), 500)
+		log.Fatalln(err)
+	}
+}
